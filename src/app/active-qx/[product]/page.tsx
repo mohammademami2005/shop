5b888a80afbbdfd37be613cf016a72b608108f5e
Mohammad Emami <162024353+mohammademami2005@users.noreply.github.com>
2025-10-25 23:35:06 +0330

    updata

--- src/app/active-qx/[product]/page.tsx
+++ src/app/active-qx/[product]/page.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default function Page({params}:{params:{product:string}}) {
     
   return (
-    <div>{params.product}</div>
+    <div>{params.product} <h1>hellooooo</h1></div>
   )
 }
