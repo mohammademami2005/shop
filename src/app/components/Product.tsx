c583f28d1dff3491212ac8086595763eff6fc9cc
Mohammad Emami <162024353+mohammademami2005@users.noreply.github.com>
2025-10-26 22:31:07 +0330

    complete store and product.tsx

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,28 +1,39 @@
 'use client'
 import { Box, Button, Container, Stack, Typography, useTheme } from '@mui/material'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AllData } from '../page'
 import { useCartStore } from '../store/store'
 
 export default function Product({ data }: { data: AllData }) {
     const [change, setChange] = useState(0)
+    const [quantity1, setQuantity] = useState(0)
+    const [btnDesabled, setBtnDesabled] = useState(false)
 
     const theme = useTheme()
 
-    const { addItem, items, removeItem } = useCartStore()
+    const { addItem, items, removeItem, quantityPlus, quantityMinus } = useCartStore()
     const productId = String(data.id)
-    console.log(items)
-    console.log(data.img)
+
+    useEffect(() => {
+        const foundItem = items.find((item) => item.id === productId)
+        if (foundItem) {
+            setQuantity(foundItem.quantity)
+        } else {
+            setQuantity(0)
+        }
+        setBtnDesabled(!foundItem || foundItem.quantity === 0)
+
+    }, [items, productId])
 
     return (
         <Container maxWidth={false} disableGutters sx={{ width: "100%", marginTop: 5 }}>
             <Stack direction={{ xs: "column", lg: "row" }} >
                 <Box display={'flex'} flexDirection={{ xs: "column", md: "row" }} width={{ xs: "100%", md: "60%" }}>
-                        <Image src={data.img[change]} width={800} height={800} alt={data.name} className='object-cover  h-[90vh]' />
+                    <Image src={data.img[change]} width={800} height={800} alt={data.name} className='object-cover  h-[90vh]' />
                     <Stack direction={{ xs: "row", lg: "column" }} order={{ xs: 2, lg: "inherit" }} width={"20%"}>
                         {data.img.map((img, k) => (
-                            <Button key={k} onClick={()=>setChange(k)}>
+                            <Button key={k} onClick={() => setChange(k)}>
                                 <Image src={img} width={100} height={100} alt={data.name} />
                             </Button>
                         ))}
@@ -35,12 +46,12 @@ export default function Product({ data }: { data: AllData }) {
 
                     <Typography variant='body2' fontSize={30}>quantity</Typography>
                     <Box >
-                        <Button sx={{ color: theme.palette.text.primary, fontSize: 22 }} variant='myCustom'>-</Button>
-                        <Typography fontSize={16} padding={5} variant='overline'>1</Typography>
-                        <Button sx={{ color: theme.palette.text.primary, fontSize: 22 }} variant='myCustom'>+</Button>
+                        <Button sx={{ color: theme.palette.text.primary, fontSize: 22 }} variant='myCustom' onClick={() => quantityMinus(productId)} disabled={btnDesabled}>-</Button>
+                        <Typography fontSize={16} padding={5} variant='overline'>{quantity1}</Typography>
+                        <Button sx={{ color: theme.palette.text.primary, fontSize: 22 }} variant='myCustom' onClick={() => quantityPlus(productId)} disabled={btnDesabled}>+</Button>
                     </Box>
                     <Box width={"100%"} display={"flex"} gap={1}>
-                        <Button variant='myCustom' sx={{ width: "65%" }} onClick={() => addItem({ id: productId, name: data.name, price: data.price, quantity: 1 })}><Typography variant='subtitle1'>add to cart</Typography></Button>
+                        <Button variant='myCustom' sx={{ width: "65%" }} onClick={() => addItem({ id: productId, name: data.name, price: data.price, quantity: 1, category: data.category, description: data.description, img: data.img, bestSells: data.bestSells })}><Typography variant='subtitle1'>add to cart</Typography></Button>
                         <Button variant='text' sx={{ border: `1px solid ${theme.palette.text.primary}`, borderRadius: 4 }}>❤️</Button>
                     </Box>
                 </Stack>
--- src/app/store/store.tsx
+++ src/app/store/store.tsx
@@ -28,34 +28,63 @@ interface CartItem {
   name: string
   price: number
   quantity: number
+  description: string
+  img:string[]
+  category:string
+  bestSells : boolean
 }
 
 interface CartState {
   items: CartItem[]
   addItem: (item: CartItem) => void
   removeItem: (id: string) => void
+  quantityPlus: (id: string) => void
+  quantityMinus: (id: string) => void
 }
 
-export const useCartStore = create <CartState>((set)=>({
-  items:[],
+export const useCartStore = create<CartState>((set, get) => ({
+  items: [],
 
-  addItem:(item)=>{
-    set((state)=>{
+  addItem: (item) => {
+    set((state) => {
       // اونایی که از قبل توی کارت وجود دارند
-      const existing = state.items.find((i)=> i.id === item.id)
-      if(existing){
+      const existing = state.items.find((i) => i.id === item.id)
+      if (existing) {
         return {
-          items : state.items.map((i)=> i.id === item.id ? {...i , quantity : i.quantity +1}: i )
+          items: state.items.map((i) => i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i)
         }
       }
-      return {items :[...state.items , {...item , quantity : 1}]}
+      return { items: [...state.items, { ...item, quantity: 1 }] }
     })
   },
 
-  removeItem : (id) =>{
-    set((state)=>{
-      return {items: state.items.filter((i)=> i.id !== id)}
+  removeItem: (id) => {
+    set((state) => {
+      return { items: state.items.filter((i) => i.id !== id) }
     })
+  },
+  quantityPlus: (id: string) => {
+    set((state) => {
+      return {
+        items: state.items.map((item) => item.id === id ? { ...item, quantity: item.quantity + 1 } : item)
+      }
+    })
+  },
+
+  quantityMinus: (id: string) => {
+    const item = get().items.find((i) => i.id === id)
+    if (!item) return item
+    if (item?.quantity == 1) {
+      get().removeItem(id)
+    } else {
+
+      set((state) => {
+        return {
+          items: state.items.map((item) => item.id === id ? { ...item, quantity: item.quantity - 1 } : item)
+        }
+      })
+    }
+
   }
 }))
 
