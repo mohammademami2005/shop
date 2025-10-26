1e7d55fa4b15a29afc81b219eac2a37f6f4d46e7
Mohammad Emami <162024353+mohammademami2005@users.noreply.github.com>
2025-10-26 20:30:14 +0330

    fix varibles

--- src/app/best-sellers/page.tsx
+++ src/app/best-sellers/page.tsx
@@ -8,7 +8,7 @@ import { getData } from '../page'
 export default async function Page() {
   const { data, dataError } = await getData("https://68fa4adfef8b2e621e7f86c5.mockapi.io/shopify/products")
    let  myData = Array.isArray(data) ? data : data? [data] : []
-  const newData = myData&& myData.filter(item=> item.bestSells === true)
+  let newData = myData&& myData.filter(item=> item.bestSells === true)
   return (
         <Container maxWidth={false} disableGutters sx={{ marginTop: 4 }} >
       <Box sx={{ paddingLeft:3 , width:"30%"}}>
